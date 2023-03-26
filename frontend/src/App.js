0979b7a9285f48b48b9d7144d0f8c20adf95f818
Kulveer Kaur <kulveerkaurpctebtech20@gmail.com>
2023-03-26 12:26:28 +0530

    Industry_Website_Initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">SS Industries</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
